b0945e227839cab0e7299910c34e4184785689ab
Sean Clarke <xseanclarke@gmail.com>
2017-07-24 00:07:05 -0400

    LogService connected to fetch and createlogs

--- app/controllers/api/logs_controller.rb
+++ app/controllers/api/logs_controller.rb
@@ -3,4 +3,18 @@ class Api::LogsController < ApplicationController
     @logs = Log.all
     render json: @logs
   end
+
+  def create
+    @log = Log.new(log_params)
+    if @log.save
+      render json: @log
+    else
+      render json: { errors: { message: "This Log failed to save."}}
+    end
+  end
+
+  private
+    def log_params
+      params.require(:log).permit(:title, :description, :location)
+    end
 end
