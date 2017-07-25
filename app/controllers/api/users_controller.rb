c36931bf794624ba23999299aa261093bd0d7785
Sean Clarke <xseanclarke@gmail.com>
2017-07-25 12:27:20 -0400

    Postman Debugging

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -4,15 +4,15 @@ class Api::UsersController < ApplicationController
   end
 
   def create
-     @user = User.new(user_params)
-     if @user.save
-       render 'users/user_with_token.json.jbuilder', user: @user
-     else
-       render json: {
-         errors: @user.errors
-       }, status: 500
-     end
-   end
+    @user = User.new(user_params)
+    if @user.save
+      render 'users/user_with_token.json.jbuilder', user: @user
+    else
+      render json: {
+      errors: @user.errors
+      }, status: 500
+    end
+  end
 
   private
     def user_params
--- app/views/users/user_with_token.json.jbuilder
+++ app/views/users/user_with_token.json.jbuilder
@@ -1,4 +1,4 @@
 json.user do
-  json.(@user, :id, :email)
+  json.(@user, :id, :name, :email)
 end
 json.token(Auth.create_token(@user.id))
