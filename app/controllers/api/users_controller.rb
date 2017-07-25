34e964c3419e85eabc5e3074883ba0eb316faf75
Sean Clarke <xseanclarke@gmail.com>
2017-07-25 11:48:02 -0400

    updated params + create if/else output for postman

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -4,20 +4,17 @@ class Api::UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
-    if @user.save
-      render 'users/user_with_token.json.jbuilder', user: @user
-    else
-      render json: {
-        errors: {
-          message: "Unable to create user. E-Mail Address already taken or is not valid"
-        }
-      }
-    end
-  end
+     @user = User.new(user_params)
+     if @user.save
+       render 'users/user_with_token.json.jbuilder', user: @user
+     else
+       render json: {
+         errors: @user.errors
+       }, status: 500
+     end
 
   private
     def user_params
-      params.require(:log).permit(:name, :email, :password, :password_confirmation)
+      params.require(:log).permit(:name, :email, :password)
     end
 end
