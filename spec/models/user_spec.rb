6a5090292c3d2a129aa65a84633a2c9c819a4ac1
Sean Clarke <xseanclarke@gmail.com>
2017-07-25 00:20:29 -0400

    installed rspec and modfied test output to red/green

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,56 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'validations' do
+
+    it 'requires an email and password upon creation' do
+      user = build(:user, email: nil, password: nil)
+
+      expect(user.valid?).to equal(false)
+      expect(user.errors.full_messages).to eq([
+        "Password can't be blank",
+        "Email can't be blank",
+        "Email is invalid"
+      ])
+    end
+
+    it 'requires that an email is unique' do
+      create(:user)
+      user = build(:user)
+
+      expect(user.valid?).to equal(false)
+      expect(user.errors.full_messages).to eq([
+        "Email has already been taken"
+      ])
+    end
+
+    it 'requires that an email is valid (contains an @ symbol and a (.com, .org, etc...)' do
+      user1 = build(:user, email: 'user.com')
+      user2 = build(:user, email: 'user@jetlog')
+      user3 = build(:user, email: 'user')
+
+      expect(user1.valid?).to equal(false)
+      expect(user1.errors.full_messages).to eq([
+        "Email is invalid"
+      ])
+      expect(user2.valid?).to equal(false)
+      expect(user3.valid?).to equal(false)
+    end
+
+  end
+
+  describe 'on save' do
+
+    it 'hashes a password' do
+      user = build(:user)
+      user.save
+
+      expect(user.password_digest).not_to equal(user.password)
+    end
+  end
+
+  describe 'relationships' do
+
+  end
 end
