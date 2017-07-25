7b3b83fd82f828699a6a2a52f1ce122ffd91bbbf
Sean Clarke <xseanclarke@gmail.com>
2017-07-25 00:47:40 -0400

    Log must be created by existing user + include location validating properly

--- spec/models/log_spec.rb
+++ spec/models/log_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Log, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'validations' do
+
+    it 'must be created by an existing user and include a location' do
+      create(:user)
+      user = build(:user)
+
+      create(:continent)
+      continent = build(:continent)
+
+      create(:location)
+      location = build(:location)
+
+      log1 = build(:log, user: nil, location: location)
+      log2 = build(:log, user: user, location_id: nil)
+
+      expect(log1.valid?).to eq(false)
+      expect(log2.valid?).to eq(false)
+      expect(log1.errors.full_messages).to eq(["User must exist"])
+      expect(log2.errors.full_messages).to eq(["Location must exist"])
+    end
+  end
 end
