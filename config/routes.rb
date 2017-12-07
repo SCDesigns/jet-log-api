695e2775507b1348c729791684b2037cf5c916a7
Sean Clarke <xseanclarke@gmail.com>
2017-12-07 12:20:24 -0500

    git push heroku master

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     resources :users
     resources :logs
-
+    
     # api/auth
     post '/auth', to: "auth#login"
     post '/auth/refresh', to: "auth#refresh"
